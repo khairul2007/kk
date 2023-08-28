368135ba0664477ed817347e83be721a40491247
khairul2007 <khairul.muktagacha@gmail.com>
2023-08-29 00:16:18 +0600

    l

--- k/src/App.js
+++ k/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul
+        className="nav nav-pills nav-fill gap-2 p-1 small bg-primary rounded-5 shadow-sm"
+        idName="pillNav2"
+        role="tablist"
+        style="--bs-nav-link-color: var(--bs-white); --bs-nav-pills-link-active-color: var(--bs-primary); --bs-nav-pills-link-active-bg: var(--bs-white);"
+      >
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link active rounded-5"
+            id="home-tab2"
+            data-bs-toggle="tab"
+            type="button"
+            role="tab"
+            aria-selected="true"
+          >
+            Home
+          </button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link rounded-5"
+            id="profile-tab2"
+            data-bs-toggle="tab"
+            type="button"
+            role="tab"
+            aria-selected="false"
+          >
+            Profile
+          </button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link rounded-5"
+            id="contact-tab2"
+            data-bs-toggle="tab"
+            type="button"
+            role="tab"
+            aria-selected="false"
+          >
+            Contact
+          </button>
+        </li>
+      </ul>
+    </>
   );
 }
 
